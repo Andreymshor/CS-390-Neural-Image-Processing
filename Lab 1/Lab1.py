05f9f264c35f06b0d557aef59a89fcdf028e52ac
Andreymshor <andreyshor1999@gmail.com>
2021-09-17 17:48:21 -0400

    Added preprocessing
    Understood how the output layer is modeled

--- Lab 1/Lab1.py
+++ Lab 1/Lab1.py
@@ -57,15 +57,27 @@ class NeuralNetwork():
             
         return layerOutputList
 
+    def predict(self):
+        prediction = self.__forward()[len(self.__forward()) - 1]
+        print(prediction)
+        return prediction
 
+
+    def __loss(self):
+        pass
+
+    def __backPropogation(self):
+        pass
 
     def __batchGenerator(self, l, n):
-        # for i in range(0, len(l), n):
-        #     yield l[i : i + n]
+        for i in range(0, len(l), n):
+            yield l[i : i + n]
         pass
     
 
-    #------------------------- TESTER METHODs ---------------------------#
+
+
+    #------------------------- TESTER METHODS ---------------------------#
 
     # tester method for making sure I printed the right number of weights
     def printWeights(self): 
@@ -75,17 +87,24 @@ class NeuralNetwork():
         for array in self.weights:
             print(f"array: {array}")
     
+    # tester method to make sure my forward list looks right
     def printForwardList(self):
         print("Output of each Layer is:")
 
         for k, array in enumerate(self.__forward()):
             print(f"Layer {k + 1}: {array}")
 
-
-
-
-
-
+    # tester method to make sure my multiplication isnt goofy for Forward
+    def checkForwardMultiplication(self):
+        currInput = self.inputs # (1 x n) matrix
+        for i,weight in enumerate(self.weights): # weight is an n x m matrix
+            print(f"Current Weight used for layer {i + 1}: {weight}")
+            print(f"Current Input used for layer {i + 1} is: {currInput}")
+            currLayer = np.dot(currInput, weight) 
+            activationList = []
+            for elem in currLayer:
+                activationList.append(self.__sigmoid(elem))
+            currInput = activationList
 
 
 
@@ -99,14 +118,46 @@ def guesserClassifier(xTest):
     return np.array(ans)
 
 
+#-----------------------------PIPELINE FUNCTIONS!------------------------------#
+
+def getRawData():
+    mnist = tf.keras.datasets.mnist
+    (xTrain, yTrain), (xTest, yTest) = mnist.load_data()
+    print("Shape of xTrain dataset: %s." % str(xTrain.shape))
+    print("Shape of yTrain dataset: %s." % str(yTrain.shape))
+    print("Shape of xTest dataset: %s." % str(xTest.shape))
+    print("Shape of yTest dataset: %s." % str(yTest.shape))
+    return ((xTrain, yTrain), (xTest, yTest))
+
+
+def preprocessData(raw):
+    ((xTrain, yTrain), (xTest, yTest)) = raw            #TODO: Add range reduction here (0-255 ==> 0.0-1.0).
+    xTrain = np.true_divide(xTrain, 255.0)
+    xTest = np.true_divide(xTest, 255.0)
+    yTrainP = to_categorical(yTrain, NUM_CLASSES) # converts to an array between 0 and 9. The ith index represents the number. A 1 represents the actual number.
+    for elem in yTrainP:
+        print(elem)
+    yTestP = to_categorical(yTest, NUM_CLASSES)
+    print("New shape of xTrain dataset: %s." % str(xTrain.shape))
+    print("New shape of xTest dataset: %s." % str(xTest.shape))
+    print("New shape of yTrain dataset: %s." % str(yTrainP.shape))
+    print("New shape of yTest dataset: %s." % str(yTestP.shape))
+    return ((xTrain, yTrainP), (xTest, yTestP))
+
+
+
 
 def main():
-    print("TESTER 1")
+
+    preprocessData(getRawData())
+    #print("TESTER 1")
     #tester = NeuralNetwork(2, 1, 3, 2)
     #tester.printWeights()
-    tester2 = NeuralNetwork([1.0,2.0],1,5,3)
-    tester2.printWeights()
-    tester2.printForwardList()
+    #tester2 = NeuralNetwork([1.0,2.0],10,5,3)
+    # tester2.printWeights()
+    # tester2.checkForwardMultiplication()
+    #tester2.predict()
+
 
 if __name__ == '__main__':
     main()
