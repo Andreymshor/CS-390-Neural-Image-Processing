76507d56f72e846ffa3fcef97a9e5611b8b7872b
Andreymshor <andreyshor1999@gmail.com>
2021-09-20 13:42:39 -0400

    Update Lab1.py

--- Lab 1/Lab1.py
+++ Lab 1/Lab1.py
@@ -17,14 +17,15 @@ ALGORITHM = "custom_net"
 
 
 class NeuralNetwork():
-    def __init__(self, inputs, outputSize, neuronsPerLayer, numLayers, learningRate = 0.1):
-        self.inputs = np.array(inputs)
-        self.inputSize = len(inputs)
-        self.outputSize = outputSize
-        self.neuronsPerLayer = neuronsPerLayer 
-        self.lr = learningRate
-        self.numLayers = numLayers
-        self.weights = []
+    def __init__(self, data, inputSize, outputSize, neuronsPerLayer, numLayers, learningRate = 0.1):
+        self.inputs = np.array(data[0]) # will be a array of arrays
+        self.labels = np.array(data[1]) # will be a array of arrays
+        self.inputSize = inputSize # used for input layer size
+        self.outputSize = outputSize # used for out put layer size
+        self.neuronsPerLayer = neuronsPerLayer # used for number of neurons in each hidden layer 
+        self.lr = learningRate # used to control the learning rate
+        self.numLayers = numLayers # used to determine number of hidden layers
+        self.weights = [] # used to initialize each layer with weights
 
         inputLayerWeights = np.random.randn(self.inputSize, self.neuronsPerLayer)
         self.weights.append(inputLayerWeights)
@@ -41,7 +42,7 @@ class NeuralNetwork():
         return 1 / (1 + np.exp(-x))
     
     def __sigmoidDerivative(self, x):
-        return self.__sigmoid(x) * (1 - self.__sigmoid(x))
+        return x * (1 - x) # because already activated
     
 
     def __forward(self): # at each layer, we dot product input and weights, and pass it into the activation function.
@@ -55,18 +56,33 @@ class NeuralNetwork():
             layerOutputList.append(activationList)
             currInput = activationList
             
-        return layerOutputList
+        return layerOutputList 
 
     def predict(self):
-        prediction = self.__forward()[len(self.__forward()) - 1]
-        print(prediction)
+        prediction = self.__forward()[len(self.__forward()) - 1] # array of 1 by 10
+
+        # predict the actual classifed value
         return prediction
 
 
-    def __loss(self):
-        pass
+    def __loss(self, predictedValues, actualValues):
+        # if predicted classifed value is = actual value, add 0
+        # else add 1
+        # cost function: (predicted - actual)^2
+        # cost for 1 specific example. Make sure to sum up all costs in a given batch and return avg
+        cost = 0
+        for i in range(len(predictedValues)):
+            cost += (predictedValues[i] - actualValues[i])**2
+        return cost
 
-    def __backPropogation(self):
+    def __backPropogation(self, layerOutputList):
+        pass
+    
+
+    # brains behind training neural network. Will use a loop to go through the examples
+    # provided in the input data and labels, with or without batches, along w/ a size 
+    # for the minibatches.
+    def train(self, epochs = 100000, minibatches = True, mbs = 100):
         pass
 
     def __batchGenerator(self, l, n):
@@ -134,25 +150,37 @@ def preprocessData(raw):
     ((xTrain, yTrain), (xTest, yTest)) = raw            #TODO: Add range reduction here (0-255 ==> 0.0-1.0).
     xTrain = np.true_divide(xTrain, 255.0)
     xTest = np.true_divide(xTest, 255.0)
+
+    finalXTrain = []
+    for matrix in xTrain:
+        finalXTrain.append(np.reshape(matrix, (matrix.shape[0] * matrix.shape[1])))
+    finalXTrain = np.array(finalXTrain)
+    
+    finalXTest = []
+    for matrix in xTest:
+        finalXTest.append(np.reshape(matrix, (matrix.shape[0] * matrix.shape[1])))
+    finalXTest = np.array(finalXTest)
     yTrainP = to_categorical(yTrain, NUM_CLASSES) # converts to an array between 0 and 9. The ith index represents the number. A 1 represents the actual number.
-    for elem in yTrainP:
-        print(elem)
+
     yTestP = to_categorical(yTest, NUM_CLASSES)
-    print("New shape of xTrain dataset: %s." % str(xTrain.shape))
-    print("New shape of xTest dataset: %s." % str(xTest.shape))
+    print("New shape of xTrain dataset: %s." % str(finalXTrain.shape))
+    print("New shape of xTest dataset: %s." % str(finalXTest.shape))
     print("New shape of yTrain dataset: %s." % str(yTrainP.shape))
     print("New shape of yTest dataset: %s." % str(yTestP.shape))
-    return ((xTrain, yTrainP), (xTest, yTestP))
+    return ((finalXTrain, yTrainP), (finalXTest, yTestP))
 
 
 
 
 def main():
 
-    preprocessData(getRawData())
+    ((xTrain, yTrainP), (xTest, yTestP)) = preprocessData(getRawData())
+    matrix1 = xTrain[0]
+    print(type(matrix1))
+    matrix2 = yTrainP[0]
+    print(matrix2)
     #print("TESTER 1")
-    #tester = NeuralNetwork(2, 1, 3, 2)
-    #tester.printWeights()
+   
     #tester2 = NeuralNetwork([1.0,2.0],10,5,3)
     # tester2.printWeights()
     # tester2.checkForwardMultiplication()
